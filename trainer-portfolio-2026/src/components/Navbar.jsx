16a5829f937f82a816433a067e1b6c3128545979
George <gulubovgeorge@gmail.com>
2026-06-16 22:21:06 +0300

    Add Navbar component with hamburger menu and animations

--- trainer-portfolio-2026/src/components/Navbar.jsx
+++ trainer-portfolio-2026/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { useState } from "react";
+import "../styles/navbar.css";
 
-export default Navbar
+const Navbar = () => {
+  let [status, setStatus] = useState(false);
+
+  const toggleMenu = () => {
+    setStatus(!status);
+  };
+
+  return (
+    <nav>
+      <div className="name">VENELIN HRISTOV</div>
+
+      <button className={status ? "btn-open" : "btn-closed"} onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <ul className={status ? "open" : "closed"}>
+        <li>About</li>
+        <li>Services</li>
+        <li>Results</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
